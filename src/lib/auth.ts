62ec2bf519f14ec04bbc050e1630c8ad372a9f9f
Alan <alxnvtn@gmail.com>
2025-05-31 19:24:46 -0300

    fix: improve clinic data handling in user session and enhance error handling in protected routes

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -28,14 +28,16 @@ export const auth = betterAuth({
       });
 
       //TODO: to adapter for user has many clinics, we need to change this
-      const clinic = clinics[0];
+      const clinic = clinics?.[0];
       return {
         user: {
           ...user,
-          clinic: {
-            id: clinic.clinicId,
-            name: clinic.clinic.name,
-          },
+          clinic: clinic?.clinicId
+            ? {
+                id: clinic?.clinicId,
+                name: clinic?.clinic?.name,
+              }
+            : undefined,
         },
         session,
       };
